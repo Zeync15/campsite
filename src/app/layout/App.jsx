da4da562e8613e75331205f72398d5627273c480
Chai Ze Yen <zeyenchai@gmail.com>
2021-04-26 15:06:16 +0800

    Add authentication in

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,9 +9,15 @@ import HomePage from "../../features/home/HomePage";
 import ModalManager from "../common/modals/ModalManager";
 import { ToastContainer } from "react-toastify";
 import ErrorComponent from "../common/errors/ErrorComponent";
+import AccountPage from "../../features/auth/AccountPage";
+import { useSelector } from "react-redux";
+import LoadingComponent from "./LoadingComponent";
 
 export default function App() {
   const { key } = useLocation();
+  const { initialized } = useSelector((state) => state.async);
+
+  if (!initialized) return <LoadingComponent content='Loading app...' />;
 
   return (
     <>
@@ -31,6 +37,7 @@ export default function App() {
                 component={EventForm}
                 key={key}
               />
+              <Route path='/account' component={AccountPage} />
               <Route path='/error' component={ErrorComponent} />
             </Container>
           </>
